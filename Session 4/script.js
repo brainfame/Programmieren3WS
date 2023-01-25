f0fe1f276354183a9a3c1882b2599204ff6d9382
brainfame <brainfameboy@tumo.world>
2023-01-25 16:12:38 +0100

    noriknofun

---
+++ Session 4/script.js
@@ -0,0 +1,33 @@
+let clickCounter = 0;
+function clickDealer(evt){
+    //console.log(evt);
+    clickCounter++;
+    let str = "pleasure doing buissnis with you " + clickCounter;
+    this.innerText = str;
+}
+
+let p = document.getElementById("pElement");
+
+p.addEventListener("click", clickDealer);
+
+function bodyClick(evt){
+    console.log("click at: ", evt.pageX, evt.PageY);
+}
+
+window.onclick = bodyClick;
+
+function pageLoaded(evt){
+    console.log("Laden fertig... bitte Spiel starten... ")
+}
+
+window.onload = pageLoaded;
+
+function keyup(evt){
+    console.log("keyboard pressed: ", evt.key);
+}
+
+window.onkeyup = keyup;
+
+function keyPressed(){
+    console.log(evt);
+}
