397be04e70aef8d59ce0b1b78e8a1a345399a261
brainfame <brainfameboy@tumo.world>
2023-02-24 18:21:36 +0100

    folder rename

--- Session 4/script.js
+++ Session 4/script.js
@@ -2,33 +2,32 @@ let clickCounter = 0;
 function clickDealer(evt){
     //console.log(evt);
     clickCounter++;
-    let str = "pleasure doing buissnis with you";
+    let str = "pleasure doing buissnis with you ";
     let str2 = clickCounter;
     this.innerText = str, str2;
 }
 
 let p = document.getElementById("pElement");
-
 p.addEventListener("click", clickDealer);
 
-function bodyClick(evt){
-    console.log("click at: ", evt.pageX, evt.PageY);
+
+
+function bodyClick(evt)
+{
+    console.log("clicked at: ", evt.pageX, evt.PageY);
 }
+window.onclick = bodyClick(); //equvalent zu .addEventListener("click", bodyClick())
 
-window.onclick = bodyClick;
 
-function pageLoaded(evt){
+function pageLoaded(evt)
+{
     console.log("Laden fertig... bitte Spiel starten... ")
 }
+window.onload = pageLoaded(); //equivalent zu addEventListener("onload", pageLoaded())
 
-window.onload = pageLoaded;
-
-function keyup(evt){
+function keydown(evt)
+{
     console.log("keyboard pressed: ", evt.key);
 }
+window.onkeydown = keydown(); //equivalent zu .addEventListener("keydown", keydown())
 
-window.onkeyup = keyup;
-
-function keyPressed(){
-    console.log(evt);
-}
